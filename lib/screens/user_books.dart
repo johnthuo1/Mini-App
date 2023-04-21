39cbfcfaf3c026cbc2e7aff8f77c400a053bff5e
johnthuo1 <90395290+johnthuo1@users.noreply.github.com>
2023-04-21 17:12:19 +0300

    Update user_books.dart

--- lib/screens/user_books.dart
+++ lib/screens/user_books.dart
@@ -1,9 +1,8 @@
-// ignore_for_file: prefer_const_constructors, unused_field
+// ignore_for_file: prefer_const_constructors, unused_field, no_leading_underscores_for_local_identifiers
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../model/user_model.dart';
-
 
 class UserBooksCollection extends StatefulWidget {
   const UserBooksCollection({Key? key}) : super(key: key);
@@ -15,33 +14,17 @@ class UserBooksCollection extends StatefulWidget {
 class _UserBooksCollectionState extends State<UserBooksCollection> {
   User? user = FirebaseAuth.instance.currentUser;
   UserModel loggedInUser = UserModel();
-  final List<String> _listItem = [];
   String? username;
   final ScrollController _scrollController = ScrollController(initialScrollOffset: 0.0);
 
-  @override
-  void initState() {
-    super.initState();
-    FirebaseFirestore.instance
-        .collection('users')
-        .doc(user!.uid)
-        .get()
-        .then((value) {
-      loggedInUser = UserModel.fromMap(value.data());
-      username = loggedInUser.userName!;
-      setState(() {});
-    });
-    fetchUserBookImages(username);
-   
-  }
-
   Future<List<String>> fetchUserBookImages(username) async {
-    QuerySnapshot<Map<String, dynamic>> querySnapshot = await FirebaseFirestore
-        .instance
-        .collection('books')
-        .where('uploadedBy', isEqualTo: username)
-        .get();
+    QuerySnapshot<Map<String, dynamic>> querySnapshot =
+        await FirebaseFirestore.instance
+            .collection('books')
+            .where('uploadedBy', isEqualTo: username)
+            .get();
 
+    List<String> _listItem = [];
 
     for (var doc in querySnapshot.docs) {
       if (doc.data().containsKey('frontImageUrl')) {
@@ -54,37 +37,66 @@ class _UserBooksCollectionState extends State<UserBooksCollection> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(user!.uid)
+        .get()
+        .then((value) {
+      loggedInUser = UserModel.fromMap(value.data());
+      setState(() {
+        username = loggedInUser.userName!;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("My Books Collection"),
       ),
-      body: _listItem.isNotEmpty ? Scrollbar(
-        controller: _scrollController,
-        child: GridView.count(
-          controller: _scrollController,
-          crossAxisCount: MediaQuery.of(context).size.width < 600 ? 2 : 4,
-          padding: EdgeInsets.all(16.0),
-          mainAxisSpacing: 16.0,
-          crossAxisSpacing: 16.0,
-          children: _listItem.map((imageUrl) {
-            return Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey,
-                  width: 1,
-                ),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Image(
-                image: NetworkImage(imageUrl),
-                width: 150,
-                height: 200,
-              ),
-            );
-          }).toList(),
-        ),
-      ) : Center(child: CircularProgressIndicator()),
+      body: username == null
+          ? Center(child: CircularProgressIndicator())
+          : FutureBuilder<List<String>>(
+              future: fetchUserBookImages(username!),
+              builder: (context, snapshot) {
+                if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+                  return Scrollbar(
+                    controller: _scrollController,
+                    child: GridView.count(
+                      controller: _scrollController,
+                      crossAxisCount:
+                          MediaQuery.of(context).size.width < 600 ? 2 : 4,
+                      padding: EdgeInsets.all(16.0),
+                      mainAxisSpacing: 16.0,
+                      crossAxisSpacing: 16.0,
+                      children: snapshot.data!.map((imageUrl) {
+                        return Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.grey,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Image(
+                            image: NetworkImage(imageUrl),
+                            width: 150,
+                            height: 200,
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  );
+                } else if (snapshot.connectionState == ConnectionState.done) {
+                  return Center(child: Text('No books found.'));
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
     );
   }
 }
