adc102304ec1ace000860fa5760ab2b8afd1800e
johnthuo1 <90395290+johnthuo1@users.noreply.github.com>
2023-04-22 17:33:28 +0300

    Update upload_profile.dart

--- lib/screens/upload_profile.dart
+++ lib/screens/upload_profile.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: camel_case_types, prefer_const_constructors, library_private_types_in_public_api, avoid_print
+// ignore_for_file: camel_case_types, prefer_const_constructors, library_private_types_in_public_api, avoid_print, use_build_context_synchronously
 
+import 'package:booksgrid/main.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
@@ -72,9 +73,23 @@ class _uploadProfileState extends State<uploadProfile> {
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: _image != null ? uploadImage : null,
+              onPressed: _image != null
+                  ? () async {
+                      await uploadImage();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Profile Picture uploaded successfully!')),
+                      );
+
+                     Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => HomePage()),
+                                  );
+                    }
+                  : null,
               child: Text('Upload'),
             ),
+
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: getImage,
