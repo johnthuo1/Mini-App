d09f8527eb37031cc37a03db7911b568d829371a
johnthuo1 <90395290+johnthuo1@users.noreply.github.com>
2023-04-23 17:40:23 +0300

    Create forgot_password.dart

---
+++ lib/screens/forgot_password.dart
@@ -0,0 +1,100 @@
+// ignore_for_file: prefer_const_constructors, library_private_types_in_public_api, avoid_print, use_build_context_synchronously
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ForgotPasswordPage extends StatefulWidget {
+  const ForgotPasswordPage({Key? key}) : super(key: key);
+
+  @override
+  _ForgotPasswordPageState createState() => _ForgotPasswordPageState();
+}
+
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final _emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _passwordReset() async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(
+        email: _emailController.text.trim(),
+      );
+      showDialog(context: context, builder: (context){
+        return AlertDialog(
+          content:Text('Check your email for the Password Reset Link'),);
+      }
+      
+      );
+    } on FirebaseAuthException catch (e) {
+      print(e);
+      showDialog(context: context, builder: (context){
+        return AlertDialog(
+          content: Text(e.message.toString())
+        );
+      }
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+      ),
+     body: Center(
+  child: SingleChildScrollView(
+    child: Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.fromLTRB(10,0,10,0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 70,
+                child: Text("Alibrary&",
+                style: GoogleFonts.arizonia(
+                  color: Colors.blue,
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),),
+              ),
+              const SizedBox(height: 16.0),
+              Text(
+                'Enter your email address and we will send you a password reset link.',
+                style: Theme.of(context).textTheme.bodyLarge,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 32.0),
+              TextField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Email Address',
+                  hintText: 'Enter your email address',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 32.0),
+              ElevatedButton(
+                onPressed: _passwordReset,
+                child: const Text('Reset Password'),
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  ),
+),
+
+    );
+  }
+}
