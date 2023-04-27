7699d2e232e4d6ec6a337cd6faae0069cac936d6
johnthuo1 <90395290+johnthuo1@users.noreply.github.com>
2023-04-27 16:56:39 +0300

    Update settings.dart

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,11 +1,17 @@
-// ignore_for_file: prefer_const_constructors, unused_import, library_private_types_in_public_api
+// ignore_for_file: prefer_const_constructors, unused_import, library_private_types_in_public_api, non_constant_identifier_names, use_build_context_synchronously
 
 import 'package:booksgrid/screens/forgot_password.dart';
+import 'package:booksgrid/screens/notifications.dart';
+import 'package:booksgrid/screens/profile.dart';
 import 'package:booksgrid/screens/sign_in.dart';
 import 'package:booksgrid/screens/upload_profile.dart';
 import 'package:booksgrid/screens/user_books.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:booksgrid/main.dart';
+
+import '../model/user_model.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -20,6 +26,24 @@ class _SettingsPageState extends State<SettingsPage> {
   String _selectedLanguage = 'English';
 
   final List<String> _languages = ['English', 'Kinyarwanda', 'French'];
+  User? user = FirebaseAuth.instance.currentUser;
+  UserModel loggedInUser = UserModel();
+  String? username;
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(user!.uid)
+        .get()
+        .then((value) {
+      loggedInUser = UserModel.fromMap(value.data());
+      setState(() {
+        username = loggedInUser.userName!;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,38 +51,303 @@ class _SettingsPageState extends State<SettingsPage> {
       appBar: AppBar(
         title: Text('Settings'),
       ),
+      // Drawer code
+      drawer: SizedBox(
+        width: 280,
+        child: Drawer(
+          elevation: 16,
+          child: Align(
+            alignment: AlignmentDirectional(-0.1, 0),
+            child: ListView(
+              padding: EdgeInsets.zero,
+              scrollDirection: Axis.vertical,
+              children: [
+                Container(
+                  width: 100,
+                  height: 217.9,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Align(
+                        alignment: AlignmentDirectional(-0.1, -0.2),
+                        child: Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(75, 30, 0, 2),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              // enable change of profile picture
+
+                              Container(
+                                width: 129,
+                                height: 129,
+                                clipBehavior: Clip.antiAlias,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                ),
+                                child: Image.network(
+                                  loggedInUser.profilePicture?.isNotEmpty ==
+                                          true
+                                      ? '${loggedInUser.profilePicture}'
+                                      : 'https://picsum.photos/seed/716/600',
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+
+                              SizedBox(width: 10),
+
+                              InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => uploadProfile()),
+                                  );
+                                },
+                                child: SizedBox(
+                                  width: 50,
+                                  height: 50,
+                                  child: Icon(
+                                    Icons.edit,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: AlignmentDirectional(-0.15, -0.25),
+                        child: Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 5),
+                          child: Text(
+                            '${loggedInUser.userName}\n${loggedInUser.email}',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 17,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentDirectional(1, 0.15),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                    child: InkWell(
+                      onTap: () async {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: ((context) => (HomePage()))));
+                      },
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.home_sharp,
+                          color: Colors.blue,
+                        ),
+                        title: Text(
+                          'Home',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.bold),
+                        ),
+                        trailing: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Color(0xFF303030),
+                          size: 20,
+                        ),
+                        tileColor: Colors.white,
+                        dense: false,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                    child: InkWell(
+                      onTap: () async {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: ((context) => (Profile()))));
+                      },
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.location_history,
+                          color: Colors.blue,
+                        ),
+                        title: Text(
+                          'Profile',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.bold),
+                        ),
+                        trailing: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Color(0xFF303030),
+                          size: 20,
+                        ),
+                        tileColor: Colors.white,
+                        dense: false,
+                      ),
+                    )),
+                Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                    child: InkWell(
+                      onTap: () async {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: ((context) => (NotificationsPage()))));
+                      },
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.notifications_active,
+                          color: Colors.blue,
+                        ),
+                        title: Text(
+                          'Notifications',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.bold),
+                        ),
+                        trailing: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Color(0xFF303030),
+                          size: 20,
+                        ),
+                        tileColor: Colors.white,
+                        dense: false,
+                      ),
+                    )),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                  child: InkWell(
+                    onTap: () async {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: ((context) => (SettingsPage()))));
+                    },
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.settings,
+                        color: Colors.blue,
+                      ),
+                      title: Text(
+                        'Settings',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 17,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios,
+                        color: Color(0xFF303030),
+                        size: 20,
+                      ),
+                      tileColor: Colors.white,
+                      dense: false,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.plumbing,
+                      color: Colors.blue,
+                    ),
+                    title: Text(
+                      'Plugins',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Icon(
+                      Icons.arrow_forward_ios,
+                      color: Color(0xFF303030),
+                      size: 20,
+                    ),
+                    tileColor: Colors.white,
+                    dense: false,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                  child: InkWell(
+                    onTap: () async {
+                      Logout(context);
+                    },
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.logout,
+                        color: Colors.blue,
+                      ),
+                      title: Text(
+                        'Log Out',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 17,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold),
+                      ),
+                      tileColor: Colors.white,
+                      dense: false,
+                      contentPadding:
+                          EdgeInsetsDirectional.fromSTEB(40, 0, 0, 0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
       body: ListView(
         children: [
-            ListTile(
+          ListTile(
             title: Text('Change Profile Picture'),
             trailing: Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Icon(Icons.account_box_rounded)),
-            onTap: 
-              () async {
-                     // Change profile picture
-                     Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => (uploadProfile())));
+                padding: EdgeInsets.all(10.0),
+                child: Icon(Icons.account_box_rounded)),
+            onTap: () async {
+              // Change profile picture
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => (uploadProfile())));
             },
-  
           ),
-
           Divider(),
           ListTile(
             title: Text('Change Password'),
-            trailing: Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Icon(Icons.key)),
-            onTap: 
-              () async {
-                            // Take User to Forgot Password Page
-                     Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => ForgotPasswordPage()));
+            trailing:
+                Padding(padding: EdgeInsets.all(10.0), child: Icon(Icons.key)),
+            onTap: () async {
+              // Take User to Forgot Password Page
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ForgotPasswordPage()));
             },
-  
           ),
           Divider(),
-
           ListTile(
             title: Text('My Books Collection'),
             trailing: Padding(
@@ -67,12 +356,13 @@ class _SettingsPageState extends State<SettingsPage> {
             ),
             onTap: () async {
               // Take User to Forgot Password Page
-              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => UserBooksCollection()));
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => UserBooksCollection()));
             },
           ),
-
           Divider(),
-
           SwitchListTile(
             title: Text('Allow Notifications'),
             value: _allowNotifications,
@@ -82,7 +372,6 @@ class _SettingsPageState extends State<SettingsPage> {
               });
             },
           ),
-
           Divider(),
           SwitchListTile(
             title: Text('Display Mode'),
@@ -118,12 +407,18 @@ class _SettingsPageState extends State<SettingsPage> {
             onTap: () async {
               // Log out user
               Navigator.pushReplacement(context,
-        MaterialPageRoute(builder: (context) => LoginScreen()));
+                  MaterialPageRoute(builder: (context) => LoginScreen()));
             },
           ),
           Divider(),
         ],
       ),
     );
+  }
+
+  Future<void> Logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => LoginScreen()));
   }
 }
