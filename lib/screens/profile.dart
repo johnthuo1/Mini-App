1f4e6ed60f27d97bfb6d86fad9c9d844964d0f34
johnthuo1 <90395290+johnthuo1@users.noreply.github.com>
2023-04-27 16:57:38 +0300

    Update profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -6,6 +6,7 @@ import 'package:booksgrid/screens/notifications.dart';
 import 'package:booksgrid/screens/settings.dart';
 import 'package:booksgrid/screens/sign_in.dart';
 import 'package:booksgrid/screens/sign_up.dart';
+import 'package:booksgrid/screens/upload_profile.dart';
 import 'package:booksgrid/screens/user_books.dart';
 import 'package:booksgrid/screens/user_information.dart';
 // import 'package:booksgrid/screens/user_books.dart';
@@ -138,7 +139,278 @@ class _ProfilePageState extends State<Profile> {
           )
         ],
       ),
-      // If necessary, you can add the drawer code here
+      
+      // drawer code 
+      drawer: SizedBox(
+        width: 280,
+        child: Drawer(
+          elevation: 16,
+          child: Align(
+            alignment: AlignmentDirectional(-0.1, 0),
+            child: ListView(
+              padding: EdgeInsets.zero,
+              scrollDirection: Axis.vertical,
+              children: [
+                Container(
+                  width: 100,
+                  height: 217.9,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Align(
+                        alignment: AlignmentDirectional(-0.1, -0.2),
+                        child: Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(75, 30, 0, 2),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              // enable change of profile picture
+ 
+                              Container(
+                                width: 129,
+                                height: 129,
+                                clipBehavior: Clip.antiAlias,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                ),
+                                child: Image.network(
+                                  loggedInUser.profilePicture?.isNotEmpty ==
+                                          true
+                                      ? '${loggedInUser.profilePicture}'
+                                      : 'https://picsum.photos/seed/716/600',
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+ 
+                              SizedBox(width: 10),
+ 
+                              InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => uploadProfile()),
+                                  );
+                                },
+                                child: SizedBox(
+                                  width: 50,
+                                  height: 50,
+                                  child: Icon(
+                                    Icons.edit,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: AlignmentDirectional(-0.15, -0.25),
+                        child: Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 5),
+                          child: Text(
+                            '${loggedInUser.userName}\n${loggedInUser.email}',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 17,
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentDirectional(1, 0.15),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                    child: InkWell(
+                      onTap: () async {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: ((context) => (HomePage()))));
+                      },
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.home_sharp,
+                          color: Colors.blue,
+                        ),
+                        title: Text(
+                          'Home',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.bold),
+                        ),
+                        trailing: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Color(0xFF303030),
+                          size: 20,
+                        ),
+                        tileColor: Colors.white,
+                        dense: false,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                    child: InkWell(
+                      onTap: () async {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: ((context) => (Profile()))));
+                      },
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.location_history,
+                          color: Colors.blue,
+                        ),
+                        title: Text(
+                          'Profile',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.bold),
+                        ),
+                        trailing: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Color(0xFF303030),
+                          size: 20,
+                        ),
+                        tileColor: Colors.white,
+                        dense: false,
+                      ),
+                    )),
+                Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                    child: InkWell(
+                      onTap: () async {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: ((context) => (NotificationsPage()))));
+                      },
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.notifications_active,
+                          color: Colors.blue,
+                        ),
+                        title: Text(
+                          'Notifications',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.bold),
+                        ),
+                        trailing: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Color(0xFF303030),
+                          size: 20,
+                        ),
+                        tileColor: Colors.white,
+                        dense: false,
+                      ),
+                    )),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                  child: InkWell(
+                    onTap: () async {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: ((context) => (SettingsPage()))));
+                    },
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.settings,
+                        color: Colors.blue,
+                      ),
+                      title: Text(
+                        'Settings',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 17,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Icon(
+                        Icons.arrow_forward_ios,
+                        color: Color(0xFF303030),
+                        size: 20,
+                      ),
+                      tileColor: Colors.white,
+                      dense: false,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.plumbing,
+                      color: Colors.blue,
+                    ),
+                    title: Text(
+                      'Plugins',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Icon(
+                      Icons.arrow_forward_ios,
+                      color: Color(0xFF303030),
+                      size: 20,
+                    ),
+                    tileColor: Colors.white,
+                    dense: false,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                  child: InkWell(
+                    onTap: () async {
+                      Logout(context);
+                    },
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.logout,
+                        color: Colors.blue,
+                      ),
+                      title: Text(
+                        'Log Out',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 17,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold),
+                      ),
+                      tileColor: Colors.white,
+                      dense: false,
+                      contentPadding:
+                          EdgeInsetsDirectional.fromSTEB(40, 0, 0, 0),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+  
       body: SingleChildScrollView(
         child: Stack(
           children: [
